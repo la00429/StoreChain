4ac35218b0c645e2e766aed05ddaf6b0a2976200
Laura Figueredo <112426346+la00429@users.noreply.github.com>
2024-02-26 21:15:55 -0500

    cargar datos desde el main

--- src/co/edu/uptc/run/Run.java
+++ src/co/edu/uptc/run/Run.java
@@ -4,10 +4,8 @@ import co.edu.uptc.presenter.Presenter;
 
 public class Run {
 	public static void main(String[] args) {
-//		Presenter presenter = new Presenter();
-//		presenter.loadData();
-		System.out.println("i");
-		System.out.println("da");
+	Presenter presenter = new Presenter();
+	presenter.loadData();
 	}
 
 }
